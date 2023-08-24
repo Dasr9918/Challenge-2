d042e96ac74981fcc5c6163d8e6b2535969d1ace
Dasr9918 <140861092+Dasr9918@users.noreply.github.com>
2023-08-24 18:15:17 -0500

    Initial commit

---
+++ reactclass1/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+/*const App = () => {
+    return(
+      <>
+      <h1>Hola Mundo!</h1>
+      <h2>Bienvenido</h2>
+      </>
+
+
+    )
+
+}
+*/
+
+const title = 'First App';
+const FirstApp = () => {
+    return(
+      <>
+      <h1> {title} </h1>
+      <span> 10 </span>
+      </>
+    )
+
+}
+export default FirstApp
